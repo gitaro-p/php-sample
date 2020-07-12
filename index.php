ccba303ebeb2bd5b48ca1bf05c94fe589bd47644
清川　裕太郎 <yuu7353@gmail.com>
2020-07-12 09:54:03 +0900

    phpカレンダー実装

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $week = '';
 
 // 第１週目：空のセルを追加
 // 例）１日が水曜日だった場合、日曜日から火曜日の３つ分の空セルを追加する
-$week .= str_repeat('<td></td>', $youbi);
+$week .= str_repeat('<td></td>', $youbi-1);
 
 for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
 
@@ -64,13 +64,10 @@ for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
     $week .= '</td>';
 
     // 週終わり、または、月終わりの場合
-    if ($youbi % 7 == 6 || $day == $day_count) {
-
-        if ($day == $day_count) {
-            // 月の最終日の場合、空セルを追加
-            // 例）最終日が木曜日の場合、金・土曜日の空セルを追加
-            $week .= str_repeat('<td></td>', 6 - ($youbi % 7));
-        }
+    if ($youbi % 7 == 0 || $day == $day_count) {
+      if ($day == $day_count && $youbi % 7 != 0) {
+          $week .= str_repeat('<td></td>', 7 - $youbi % 7);
+      }
 
         // weeks配列にtrと$weekを追加する
         $weeks[] = '<tr>' . $week . '</tr>';
@@ -105,11 +102,11 @@ for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
         .today {
             background: orange;
         }
-        th:nth-of-type(1), td:nth-of-type(1) {
-            color: red;
+        th:nth-of-type(6), td:nth-of-type(6) {
+            color: blue;
         }
         th:nth-of-type(7), td:nth-of-type(7) {
-            color: blue;
+            color: red;
         }
     </style>
 </head>
@@ -118,13 +115,13 @@ for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
         <h3><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</a></h3>
         <table class="table table-bordered">
             <tr>
-                <th>日</th>
                 <th>月</th>
                 <th>火</th>
                 <th>水</th>
                 <th>木</th>
                 <th>金</th>
                 <th>土</th>
+                <th>日</th>
             </tr>
             <?php
                 foreach ($weeks as $week) {
